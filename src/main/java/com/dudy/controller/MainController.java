91f4fbb85daefd4a2233946a57896a3c0388920e
dudy <adam97dudek@gmail.com>
2019-01-09 12:16:06 +0100

    Route form added

--- src/main/java/com/dudy/controller/MainController.java
+++ src/main/java/com/dudy/controller/MainController.java
@@ -1,8 +1,10 @@
 package com.dudy.controller;
 
+import com.dudy.entity.MountainGroup;
 import com.dudy.entity.Point;
 import com.dudy.entity.Region;
 import com.dudy.entity.Route;
+import com.dudy.repository.MountainGroupRepository;
 import com.dudy.repository.PointRepository;
 import com.dudy.repository.RegionRepository;
 import com.dudy.repository.RouteRepository;
@@ -31,52 +33,52 @@ public class MainController {
 
     @Autowired
     private RegionRepository regionRepository;
-//    @GetMapping(path = "/add")
-//    public @ResponseBody
-//    String addNewUser () {
-//        // @ResponseBody means the returned String is the response, not a view name
-//        // @RequestParam means it is a parameter from the GET or POST request
-//
-//        Point p = new Point();
-//        p.setName("Tarnica");
-//        p.setHeight(1346);
-//        p.setLatitude(49.04);
-//        p.setLongitude(22.43);
-//        p.setDescription("Najwyższy szczyt Bieszczad");
-//        pointRepository.save(p);
-//        return "Saved";
-//    }
-@GetMapping
-@RequestMapping(value = "/", method = RequestMethod.GET)
-public String index()
-{
-    return "index";
-}
-@GetMapping
-@RequestMapping(value = "/new-point", method = RequestMethod.GET)
-public String newPoint(Model model) {
-    Point thePoint = new Point();
-    ArrayList<Region> regions = (ArrayList<Region>) regionRepository.findAll();
-    //ArrayList<Route> routes = (ArrayList<Route>) routeRepository.getRoutesByGroup("Czechowice Dziedzice");
-    Route r = routeRepository.getRoutesTest("Czechowice Dziedzice");
 
-    model.addAttribute("testRoute", r);
-    //model.addAttribute("routes", routes);
-    model.addAttribute("regions", regions);
-    model.addAttribute("point", thePoint);
-    return "point-form";
-}
+    @Autowired
+    private MountainGroupRepository mountainGroupRepository;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/new-point", method = RequestMethod.GET)
+    public String newPoint(Model model) {
+        Point thePoint = new Point();
+        ArrayList<Region> regions = (ArrayList<Region>) regionRepository.findAll();
+        Route r = routeRepository.getRoutesTest("Czechowice Dziedzice");
+
+        model.addAttribute("testRoute", r);
+        model.addAttribute("regions", regions);
+        model.addAttribute("point", thePoint);
+        return "point-form";
+    }
+
     @InitBinder
     public void initBinder(WebDataBinder binder) {
         NumberFormatUtil.registerDoubleFormat(binder);
     }
+
     @RequestMapping(value = "/savePoint", method = RequestMethod.POST)
-    public String processForm(@Valid @ModelAttribute Point point, BindingResult errors){
+    public String processForm(@Valid @ModelAttribute Point point, BindingResult errors) {
         System.out.println(errors.hasErrors());
         if (errors.hasErrors())
             return "point-form";
         pointRepository.save(point);
         return "redirect:/";
     }
+
+    @RequestMapping(value = "/routes-page", method = RequestMethod.GET)
+    public String showRoutesPage(Model model){
+        ArrayList<MountainGroup> mountainGroups = (ArrayList<MountainGroup>) mountainGroupRepository.findAll();
+        model.addAttribute("groups", mountainGroups);
+        return "show-routes";
+    }
+    @RequestMapping(value = "/processGroup", method = RequestMethod.POST)
+    public String processRouteForm(@RequestParam() String groupChosen){
+        System.out.println(groupChosen);
+        return "redirect:/demo/routes-page";
+    }
+
 
 }
--- src/main/java/com/dudy/repository/PointRepository.java
+++ src/main/java/com/dudy/repository/PointRepository.java
@@ -3,9 +3,10 @@ package com.dudy.repository;
 import com.dudy.entity.Point;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * Created by admin on 12.12.2018.
  */
 public interface PointRepository extends CrudRepository<Point , Integer>{
-
 }
