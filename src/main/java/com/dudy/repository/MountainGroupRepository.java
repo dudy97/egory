86aa12f0e1e8abe3551bc0e29e0d1033bf1d78de
dudy <adam97dudek@gmail.com>
2019-01-09 12:16:33 +0100

    new files

---
+++ src/main/java/com/dudy/repository/MountainGroupRepository.java
@@ -0,0 +1,10 @@
+package com.dudy.repository;
+
+import com.dudy.entity.MountainGroup;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by admin on 09.01.2019.
+ */
+public interface MountainGroupRepository extends CrudRepository<MountainGroup, Integer> {
+}
